8076f950e39bd606cbd1a5f37c0463bc65a63bf1
a.milenin <azrr.mail@gmail.com>
2016-10-02 15:40:34 +0300

    init

---
+++ src/Component/Cron/Scheduler.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @date    23.07.2015
+ * @version 0.1
+ * @author  Aleksandr Milenin admin@azrr.info
+ */
+
+namespace Azurre\Component\Cron;
+
+use Yalesov\CronExprParser\Parser;
+
+class Cron {
+
+    protected
+        $time,
+        $pathToJobs = '',
+        $jobs = [];
+
+    /**
+     * @param mixed $time
+     */
+    public function __construct($time = null)
+    {
+        $this->time = $time ? $time : time();
+    }
+
+    /**
+     * @param string $path
+     * @return $this
+     */
+    public function setJobPath($path)
+    {
+        $this->pathToJobs = $path;
+
+        return $this;
+    }
+
+    /**
+     * @param string   $expr
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function addJob($expr, $callback)
+    {
+        $this->jobs[] = ['expr' => $expr, 'callback' => $callback];
+
+        return $this;
+    }
+
+    public function clearJobs()
+    {
+        $this->jobs[] = [];
+
+        return $this;
+    }
+
+
+    /**
+     * Run matched jobs
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function run()
+    {
+        foreach ($this->jobs as $job) {
+            if (Parser::matchTime($this->time, $job['expr'])) {
+                call_user_func_array($job['callback'], [$this->pathToJobs]);
+            }
+        }
+
+        return $this;
+    }
+}
